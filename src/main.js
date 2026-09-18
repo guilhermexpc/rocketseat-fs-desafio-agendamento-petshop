6b80cdf6c87f53cd5f6c0a0d4fbcb45f8e81f18a
Guilherme Campos <guilhermee1219@gmail.com>
2026-09-18 14:33:55 -0300

    add dialog modal form

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById("schedule-modal");
+const openBtn = document.getElementById("open-modal");
+const closeBtn = document.getElementById("closeModal");
+
+// Método showModal() abre o dialog como modal e adiciona o ::backdrop
+openBtn.addEventListener("click", () => {
+  modal.showModal();
+});
+
+// Método close() fecha o dialog
+closeBtn.addEventListener("click", () => {
+  modal.close();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // openBtn.click();
+  // modal.showModal();
+});
